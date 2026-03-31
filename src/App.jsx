428b79d1d1a3b211963ad8c756deef73ef55088f
Abdul Baset Shahid <abdul.baset@iqvia.com>
2026-03-31 20:37:33 +0600

    restructure all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,9 @@
-import UserDetail from './components/UserDetail';
-import Dashboard from './components/Dashboard';
 import './App.css'
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router';
-import Header from './components/Header';
-import Login from './components/Login';
-import Users from './components/Users';
-import Nav from './components/Nav';
-
-function AppLayout() {
-  return (
-    <div className="App">
-      <Header />
-      <Nav />
-      <Outlet />
-    </div>
-  );
-}
-
-var routes = createBrowserRouter([
-  {
-    path: '/',
-    Component: AppLayout,
-    children: [
-      { index: true, Component: Dashboard },
-      { path: 'login', Component: Login },
-      { path: 'users', Component: Users },
-      { path: 'users/:id', Component: UserDetail },
-    ],
-  },
-]);
+import { RouterProvider } from 'react-router';
+import { router } from './app/router/router';
 
 function App() {
-  return <RouterProvider router={routes} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App
